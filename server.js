62adab99ab6017565aa5be1d08ac2c2987c57882
Ultrafeeder <jakarimstratton@gmail.com>
2022-03-14 20:31:32 -0400

    connected database

--- server.js
+++ server.js
@@ -5,12 +5,33 @@ const mysql = require('mysql');
 const app = express();
 
 //connecting to the database and checking for errors if any are present
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Skript@2744',
+    database: 'skylark_demo'
+});
 
+db.connect(err => {
+    if (err) {
+        return err
+    } else {
+        console.log('SUCCESS')
+    }
+});
 
 app.use(cors());
 app.use(express.json());
 
-
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result);
+        }
+    })
+})
 
 app.listen(4000, () => {
     console.log(`console server listening on port 4000`)
